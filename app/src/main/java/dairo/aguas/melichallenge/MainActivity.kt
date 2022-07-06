f523683a215103fc57e9d69173a6691dde4321b8
Dairo Aguas B <ingdairoaguas@gmail.com>
2022-07-06 16:57:34 -0500

    Added SearchLayout

--- app/src/main/java/dairo/aguas/melichallenge/MainActivity.kt
+++ app/src/main/java/dairo/aguas/melichallenge/MainActivity.kt
@@ -3,41 +3,13 @@ package dairo.aguas.melichallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dairo.aguas.melichallenge.ui.theme.MeliChallengeTheme
+import dairo.aguas.melichallenge.ui.home.HomeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MeliChallengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Dairo")
-                }
-            }
+            HomeApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MeliChallengeTheme {
-        Greeting("Dairo")
     }
 }

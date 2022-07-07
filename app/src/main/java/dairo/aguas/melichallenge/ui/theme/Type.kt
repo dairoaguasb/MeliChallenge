4d048578fd019df60b2e0762c4156ded0f7d1da6
Dairo Aguas B <ingdairoaguas@gmail.com>
2022-07-06 20:33:01 -0500

    Added CardProduct and ProductViewData

--- app/src/main/java/dairo/aguas/melichallenge/ui/theme/Type.kt
+++ app/src/main/java/dairo/aguas/melichallenge/ui/theme/Type.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp
     )
 )

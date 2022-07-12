4aae9047ad104d2b1e16cb838b66cae74358a450
Dairo Aguas Barraza <ingdairoaguas@gmail.com>
2022-07-12 11:15:29 -0500

    Look and feel detail (#8)
    * Created DetailScreen and DetailViewData
    
    * Created CardProductHorizontal and finished DetailScreen
    
    * Business logic detail (#7)
    
    * Created ProductDetailRepository and DTO
    
    * Showing information in DetailScreen
    
    * Showing product description
    
    * Showing seller products

---
+++ app/src/main/java/dairo/aguas/melichallenge/data/repository/ProductDetailRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package dairo.aguas.melichallenge.data.repository
+
+import dairo.aguas.melichallenge.data.endpoint.ProductAPI
+import dairo.aguas.melichallenge.domain.model.Product
+import dairo.aguas.melichallenge.domain.model.ProductDetail
+import dairo.aguas.melichallenge.domain.model.Result
+import dairo.aguas.melichallenge.domain.repository.DomainExceptionRepository
+import dairo.aguas.melichallenge.domain.repository.ProductDetailRepository
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+class ProductDetailRepositoryImpl(
+    private val productAPI: ProductAPI,
+    private val domainExceptionRepository: DomainExceptionRepository
+) : ProductDetailRepository {
+
+    override fun getProductDetail(id: String) = flow<Result<ProductDetail>> {
+        val apiResult = productAPI.getProductDetail(id)
+        val apiResultDescription = productAPI.getProductDescription(id)
+        emit(Result.Success(apiResult.toDomainProductDetail(apiResultDescription.description)))
+    }.catch {
+        emit(Result.Failure(domainExceptionRepository.manageError(it)))
+    }
+
+    override fun getProductListSeller(sellerId: Int) = flow<Result<List<Product>>> {
+        val apiResult = productAPI.getProductListSeller(sellerId)
+        emit(Result.Success(apiResult.productList.map { it.toDomainProduct() }))
+    }.catch {
+        emit(Result.Failure(domainExceptionRepository.manageError(it)))
+    }
+}
--- app/src/main/java/dairo/aguas/melichallenge/domain/exception/ExceptionHandler.kt
+++ app/src/main/java/dairo/aguas/melichallenge/domain/exception/ExceptionHandler.kt
@@ -10,6 +10,7 @@ class ExceptionHandler {
             is InternalErrorException -> R.string.error_internal_error_exception
             is ParseException -> R.string.error_parsing_error
             is NoConnectivityDomainException -> R.string.error_internet_connection
+            is NotFoundException -> R.string.error_http_not_found
             else -> R.string.error_some_wrong
         }
 }
